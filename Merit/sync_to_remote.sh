d79e7935a6f9d0f8f673da1dee88d1308968fd67
lsmin3388 <lsmin3388_@naver.com>
2025-11-17 15:32:50 +0900

    Update: 모델 경로 수정 및 동기화 스크립트에 models 폴더 제외 추가

--- Merit/sync_to_remote.sh
+++ Merit/sync_to_remote.sh
@@ -6,7 +6,7 @@ REMOTE_PATH="~/Merit"
 LOCAL_PATH="/Users/isangmin/Desktop/종합설계프로젝트/paper_review/Merit"
 
 echo "Syncing Merit from local to remote server..."
-rsync -avz --exclude '.git' --exclude '__pycache__' --exclude '*.pyc' \
+rsync -avz --exclude '.git' --exclude '__pycache__' --exclude '*.pyc' --exclude 'models' \
     ${LOCAL_PATH}/ ${REMOTE_HOST}:${REMOTE_PATH}/
 
 echo "Sync completed!"
