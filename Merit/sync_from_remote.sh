39ff56e9924a6510be932d76f266ac637365fb0e
lsmin3388 <lsmin3388_@naver.com>
2025-11-17 15:33:25 +0900

    Fix: 동기화 스크립트 exclude 경로 수정 (/models)

--- Merit/sync_from_remote.sh
+++ Merit/sync_from_remote.sh
@@ -6,7 +6,7 @@ REMOTE_PATH="~/Merit"
 LOCAL_PATH="/Users/isangmin/Desktop/종합설계프로젝트/paper_review/Merit"
 
 echo "Syncing Merit from remote server to local..."
-rsync -avz --exclude '.git' --exclude '__pycache__' --exclude '*.pyc' --exclude 'models' \
+rsync -avz --exclude '.git' --exclude '__pycache__' --exclude '*.pyc' --exclude '/models' \
     ${REMOTE_HOST}:${REMOTE_PATH}/ ${LOCAL_PATH}/
 
 echo "Sync completed!"
