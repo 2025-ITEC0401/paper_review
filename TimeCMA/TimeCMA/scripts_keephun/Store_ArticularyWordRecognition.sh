de4c5662254b5aaa67aa05b522140411272cef69
wlgns12370 <knu12370@knu.ac.kr>
2025-11-26 21:11:25 +0900

    Feat: ArticularyWordRecognition 임베딩 저장

---
+++ TimeCMA/TimeCMA/scripts_keephun/Store_ArticularyWordRecognition.sh
@@ -0,0 +1,26 @@
+ #!/bin/bash
+export PYTHONPATH=/hdd/intern/daniel/TimeCMA/sfs-common/cxliu/TimeCMA:$PYTHONPATH
+
+
+data_paths=("ArticularyWordRecognition_m")
+divides=("train" "val" "test")
+num_nodes=10
+input_len=96
+output_len=96
+
+for data_path in "${data_paths[@]}"; do
+  divide="train"
+  log_file="./Results/emb_logs/${data_path}_${divide}.log"
+  export CUDA_VISIBLE_DEVICES=1
+  nohup /hdd/conda_envs/envs/TimeCMA/bin/python storage/store_emb.py --divide $divide --data_path $data_path --num_nodes $num_nodes --input_len $input_len --output_len $output_len > $log_file &
+
+  divide="val"
+  log_file="./Results/emb_logs/${data_path}_${divide}.log"
+  export CUDA_VISIBLE_DEVICES=1
+  nohup /hdd/conda_envs/envs/TimeCMA/bin/python storage/store_emb.py --divide $divide --data_path $data_path --num_nodes $num_nodes --input_len $input_len --output_len $output_len > $log_file &
+
+  divide="test"
+  log_file="./Results/emb_logs/${data_path}_${divide}.log"
+  export CUDA_VISIBLE_DEVICES=1
+  nohup /hdd/conda_envs/envs/TimeCMA/bin/python storage/store_emb.py --divide $divide --data_path $data_path --num_nodes $num_nodes --input_len $input_len --output_len $output_len > $log_file &
+done
