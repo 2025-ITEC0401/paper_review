6bc8912ae60c8af3b47ad51dd0adc43bb9fad7a2
wlgns12370 <knu12370@knu.ac.kr>
2025-12-04 14:03:52 +0900

    feat: classification_ArticularyWordRecognition 구현

---
+++ TimeCMA/TimeCMA/scripts_keephun/classification_ArticularyWordRecognition_run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export CUDA_VISIBLE_DEVICES=0,1
+
+echo "=================================================="
+echo "Start Classification: AtrialFibrillation_m (SVM)"
+echo "Date: $(date)"
+echo "=================================================="
+
+/hdd/conda_envs/envs/TimeCMA/bin/python downstream_classification.py \
+  --data_path ArticularyWordRecognition_m \
+  --root_path "/hdd/intern/keephun/TimeCMA/sfs-common/dataset/" \
+  --batch_size 64 \
+  --num_nodes 10 \
+  --seq_len 96 \
+  --pred_len 96 \
+  --channel 64 \
+  --dropout_n 0.7 \
+  --e_layer 1 \
+  --d_layer 2 \
+  --checkpoint "/hdd/intern/keephun/TimeCMA/logs/2025-12-02-17:18:58-/ArticularyWordRecognition_m/96_64_1_2_0.0001_0.7_2024/best_model.pth" \
+  --classifier svm \
+  --target_col OT \
+  --feature_type latent \
+  --output_dir "./Results/classification_comparison/ArticularyWordRecognition_m/" \
+  --device cuda:1
+
+echo "=================================================="
+echo "Process Finished."
+echo "=================================================="
